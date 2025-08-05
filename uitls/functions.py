b9d4236b29abab248f181250a58ac533359e2e37
NVSI_SJGLAB <115686053+SJGLAB@users.noreply.github.com>
2025-08-05 23:03:53 +0800

    Add files via upload

---
+++ uitls/functions.py
@@ -0,0 +1,378 @@
+# -*- coding: utf-8 -*-
+# @Author: Jie
+# @Date:   2017-06-15 14:23:06
+# @Last Modified by:   Jie Yang,     Contact: jieynlp@gmail.com
+# @Last Modified time: 2018-06-10 17:49:50
+from __future__ import print_function
+from __future__ import absolute_import
+import sys
+import numpy as np
+import torch
+import torch.autograd as autograd
+
+def normalize_word(word):
+    new_word = ""
+    for char in word:
+        if char.isdigit():
+            new_word += '0'
+        else:
+            new_word += char
+    return new_word
+
+
+def read_instance(input_file, word_alphabet, char_alphabet, feature_alphabets, label_alphabet, number_normalized, max_sent_length, char_padding_size=-1, char_padding_symbol = '</pad>'):
+    feature_num = len(feature_alphabets)
+    in_lines = open(input_file,'r').readlines()
+    instence_texts = []
+    instence_Ids = []
+    words = []
+    features = []
+    chars = []
+    labels = []
+    word_Ids = []
+    feature_Ids = []
+    char_Ids = []
+    label_Ids = []
+    for line in in_lines:
+        if len(line) > 2:
+            pairs = line.strip().split()
+
+            if sys.version_info[0] < 3:
+                word = pairs[0].decode('utf-8')
+            else:
+                word = pairs[0]
+
+            if number_normalized:
+                word = normalize_word(word)
+            label = pairs[-1]
+            words.append(word)
+            labels.append(label)
+            word_Ids.append(word_alphabet.get_index(word))
+            # print(label_alphabet)
+            label_Ids.append(label_alphabet.get_index(label))
+            ## get features
+            feat_list = []
+            feat_Id = []
+            for idx in range(feature_num):
+                feat_idx = pairs[idx+1].split(']',1)[-1]
+                feat_list.append(feat_idx)
+                feat_Id.append(feature_alphabets[idx].get_index(feat_idx))
+            features.append(feat_list)
+            feature_Ids.append(feat_Id)
+            ## get char
+            char_list = []
+            char_Id = []
+            for char in word:
+                char_list.append(char)
+            if char_padding_size > 0:
+                char_number = len(char_list)
+                if char_number < char_padding_size:
+                    char_list = char_list + [char_padding_symbol]*(char_padding_size-char_number)
+                assert(len(char_list) == char_padding_size)
+            else:
+                ### not padding
+                pass
+            for char in char_list:
+                char_Id.append(char_alphabet.get_index(char))
+            chars.append(char_list)
+            char_Ids.append(char_Id)
+        else:
+            if (len(words) > 0) and ((max_sent_length < 0) or (len(words) < max_sent_length)) :
+                instence_texts.append([words, features, chars, labels])
+                instence_Ids.append([word_Ids, feature_Ids, char_Ids,label_Ids])
+            words = []
+            features = []
+            chars = []
+            labels = []
+            word_Ids = []
+            feature_Ids = []
+            char_Ids = []
+            label_Ids = []
+    return instence_texts, instence_Ids
+
+def read_test(input_file, word_alphabet, char_alphabet, feature_alphabets, label_alphabet, number_normalized, max_sent_length, char_padding_size=-1, char_padding_symbol = '</pad>'):
+    feature_num = len(feature_alphabets)
+    in_lines = open(input_file,'r').readlines()
+    instence_texts = []
+    instence_Ids = []
+    words = []
+    features = []
+    chars = []
+    labels = []
+    word_Ids = []
+    feature_Ids = []
+    char_Ids = []
+    label_Ids = []
+    for line in in_lines:
+        if len(line) > 1:
+            line = line.strip() + ' ATG'
+            pairs = line.strip().split()
+
+            if sys.version_info[0] < 3:
+                word = pairs[0].decode('utf-8')
+            else:
+                word = pairs[0]
+
+            if number_normalized:
+                word = normalize_word(word)
+            label = pairs[-1]
+            words.append(word)
+            labels.append(label)
+            word_Ids.append(word_alphabet.get_index(word))
+            # print(label_alphabet)
+            label_Ids.append(label_alphabet.get_index(label))
+            ## get features
+            feat_list = []
+            feat_Id = []
+            for idx in range(feature_num):
+                feat_idx = pairs[idx+1].split(']',1)[-1]
+                feat_list.append(feat_idx)
+                feat_Id.append(feature_alphabets[idx].get_index(feat_idx))
+            features.append(feat_list)
+            feature_Ids.append(feat_Id)
+            ## get char
+            char_list = []
+            char_Id = []
+            for char in word:
+                char_list.append(char)
+            if char_padding_size > 0:
+                char_number = len(char_list)
+                if char_number < char_padding_size:
+                    char_list = char_list + [char_padding_symbol]*(char_padding_size-char_number)
+                assert(len(char_list) == char_padding_size)
+            else:
+                ### not padding
+                pass
+            for char in char_list:
+                char_Id.append(char_alphabet.get_index(char))
+            chars.append(char_list)
+            char_Ids.append(char_Id)
+        else:
+            if (len(words) > 0) and ((max_sent_length < 0) or (len(words) < max_sent_length)) :
+                instence_texts.append([words, features, chars, labels])
+                instence_Ids.append([word_Ids, feature_Ids, char_Ids,label_Ids])
+            words = []
+            features = []
+            chars = []
+            labels = []
+            word_Ids = []
+            feature_Ids = []
+            char_Ids = []
+            label_Ids = []
+    return instence_texts, instence_Ids
+
+
+
+def build_pretrain_embedding(embedding_path, word_alphabet, embedd_dim=50, norm=True):
+    embedd_dict = dict()
+    if embedding_path != None:
+        embedd_dict, embedd_dim = load_pretrain_emb(embedding_path)
+    alphabet_size = word_alphabet.size()
+    scale = np.sqrt(3.0 / embedd_dim)
+    pretrain_emb = np.empty([word_alphabet.size(), embedd_dim])
+    perfect_match = 0
+    case_match = 0
+    not_match = 0
+    for word, index in word_alphabet.iteritems():
+        if word in embedd_dict:
+            if norm:
+                pretrain_emb[index,:] = norm2one(embedd_dict[word])
+            else:
+                pretrain_emb[index,:] = embedd_dict[word]
+            perfect_match += 1
+        elif word.lower() in embedd_dict:
+            if norm:
+                pretrain_emb[index,:] = norm2one(embedd_dict[word.lower()])
+            else:
+                pretrain_emb[index,:] = embedd_dict[word.lower()]
+            case_match += 1
+        else:
+            pretrain_emb[index,:] = np.random.uniform(-scale, scale, [1, embedd_dim])
+            not_match += 1
+    pretrained_size = len(embedd_dict)
+    print("Embedding:\n     pretrain word:%s, prefect match:%s, case_match:%s, oov:%s, oov%%:%s"%(pretrained_size, perfect_match, case_match, not_match, (not_match+0.)/alphabet_size))
+    return pretrain_emb, embedd_dim
+
+def norm2one(vec):
+    root_sum_square = np.sqrt(np.sum(np.square(vec)))
+    return vec/root_sum_square
+
+def load_pretrain_emb(embedding_path):
+    embedd_dim = -1
+    embedd_dict = dict()
+    with open(embedding_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            tokens = line.split()
+            if embedd_dim < 0:
+                embedd_dim = len(tokens) - 1
+            else:
+                assert (embedd_dim + 1 == len(tokens))
+            embedd = np.empty([1, embedd_dim])
+            embedd[:] = tokens[1:]
+            if sys.version_info[0] < 3:
+                first_col = tokens[0].decode('utf-8')
+            else:
+                first_col = tokens[0]
+            embedd_dict[first_col] = embedd
+    return embedd_dict, embedd_dim
+
+def batchify_with_label(input_batch_list, gpu,label_size = 20, volatile_flag=False):
+    """
+        input: list of words, chars and labels, various length. [[words,chars, labels],[words,chars,labels],...]
+            words: word ids for one sentence. (batch_size, sent_len)
+            chars: char ids for on sentences, various length. (batch_size, sent_len, each_word_length)
+        output:
+            zero padding for word and char, with their batch length
+            word_seq_tensor: (batch_size, max_sent_len) Variable
+            word_seq_lengths: (batch_size,1) Tensor
+            char_seq_tensor: (batch_size*max_sent_len, max_word_len) Variable
+            char_seq_lengths: (batch_size*max_sent_len,1) Tensor
+            char_seq_recover: (batch_size*max_sent_len,1)  recover char sequence order
+            label_seq_tensor: (batch_size, max_sent_len)
+            mask: (batch_size, max_sent_len)
+    """
+    #label_instance
+    batch_size = len(input_batch_list)
+    words = [sent[0] for sent in input_batch_list]
+    features = [np.asarray(sent[1]) for sent in input_batch_list]
+    feature_num = len(features[0][0])
+    chars = [sent[2] for sent in input_batch_list]
+    labels = [sent[3] for sent in input_batch_list]
+
+    word_seq_lengths = torch.LongTensor(list(map(len, words)))
+    max_seq_len = word_seq_lengths.max()
+    word_seq_tensor = autograd.Variable(torch.zeros((batch_size, max_seq_len)), volatile =  volatile_flag).long()
+    label_seq_tensor = autograd.Variable(torch.zeros((batch_size, max_seq_len)),volatile =  volatile_flag).long()
+    #
+    input_label_seq_tensor = autograd.Variable(torch.zeros((batch_size, label_size)),volatile =  volatile_flag).long()
+
+    feature_seq_tensors = []
+    for idx in range(feature_num):
+        feature_seq_tensors.append(autograd.Variable(torch.zeros((batch_size, max_seq_len)),volatile =  volatile_flag).long())
+    mask = autograd.Variable(torch.zeros((batch_size, max_seq_len)),volatile =  volatile_flag).byte()
+    for idx, (seq, label, seqlen) in enumerate(zip(words, labels, word_seq_lengths)):
+        word_seq_tensor[idx, :seqlen] = torch.LongTensor(seq)
+        label_seq_tensor[idx, :seqlen] = torch.LongTensor(label)
+        mask[idx, :seqlen] = torch.Tensor([1]*seqlen)
+        input_label_seq_tensor[idx, :label_size] = torch.LongTensor([i for i in range(label_size)])
+        for idy in range(feature_num):
+            feature_seq_tensors[idy][idx,:seqlen] = torch.LongTensor(features[idx][:,idy])
+
+
+    word_seq_lengths, word_perm_idx = word_seq_lengths.sort(0, descending=True)
+    word_seq_tensor = word_seq_tensor[word_perm_idx]
+    for idx in range(feature_num):
+        feature_seq_tensors[idx] = feature_seq_tensors[idx][word_perm_idx]
+
+    label_seq_tensor = label_seq_tensor[word_perm_idx]
+    mask = mask[word_perm_idx]
+    ### deal with char
+    # pad_chars (batch_size, max_seq_len)
+    pad_chars = [chars[idx] + [[0]] * (max_seq_len-len(chars[idx])) for idx in range(len(chars))]
+    length_list = [list(map(len, pad_char)) for pad_char in pad_chars]
+    max_word_len = max(map(max, length_list))
+    char_seq_tensor = autograd.Variable(torch.zeros((batch_size, max_seq_len, max_word_len)), volatile =  volatile_flag).long()
+    char_seq_lengths = torch.LongTensor(length_list)
+    for idx, (seq, seqlen) in enumerate(zip(pad_chars, char_seq_lengths)):
+        for idy, (word, wordlen) in enumerate(zip(seq, seqlen)):
+            # print len(word), wordlen
+            char_seq_tensor[idx, idy, :wordlen] = torch.LongTensor(word)
+
+    char_seq_tensor = char_seq_tensor[word_perm_idx].view(batch_size*max_seq_len,-1)
+    char_seq_lengths = char_seq_lengths[word_perm_idx].view(batch_size*max_seq_len,)
+    char_seq_lengths, char_perm_idx = char_seq_lengths.sort(0, descending=True)
+    char_seq_tensor = char_seq_tensor[char_perm_idx]
+    _, char_seq_recover = char_perm_idx.sort(0, descending=False)
+    _, word_seq_recover = word_perm_idx.sort(0, descending=False)
+    if gpu:
+        word_seq_tensor = word_seq_tensor.cuda()
+        for idx in range(feature_num):
+            feature_seq_tensors[idx] = feature_seq_tensors[idx].cuda()
+        word_seq_lengths = word_seq_lengths.cuda()
+        word_seq_recover = word_seq_recover.cuda()
+        label_seq_tensor = label_seq_tensor.cuda()
+        char_seq_tensor = char_seq_tensor.cuda()
+        char_seq_recover = char_seq_recover.cuda()
+        input_label_seq_tensor = input_label_seq_tensor.cuda()
+        mask = mask.cuda()
+
+
+    return word_seq_tensor,feature_seq_tensors, word_seq_lengths, word_seq_recover, char_seq_tensor, char_seq_lengths, char_seq_recover, label_seq_tensor, mask ,input_label_seq_tensor
+
+def predict_check(pred_variable, gold_variable, mask_variable):
+    """
+        input:
+            pred_variable (batch_size, sent_len): pred tag result, in numpy format
+            gold_variable (batch_size, sent_len): gold result variable
+            mask_variable (batch_size, sent_len): mask variable
+    """
+    pred = pred_variable.cpu().data.numpy()
+    gold = gold_variable.cpu().data.numpy()
+    mask = mask_variable.cpu().data.numpy()
+    overlaped = (pred == gold)
+    right_token = np.sum(overlaped * mask)
+    total_token = mask.sum()
+    # print("right: %s, total: %s"%(right_token, total_token))
+    return right_token, total_token
+
+
+def recover_label(pred_variable, gold_variable, mask_variable, label_alphabet, word_recover):
+    """
+        input:
+            pred_variable (batch_size, sent_len): pred tag result
+            gold_variable (batch_size, sent_len): gold result variable
+            mask_variable (batch_size, sent_len): mask variable
+    """
+
+    pred_variable = pred_variable[word_recover]
+    gold_variable = gold_variable[word_recover]
+    mask_variable = mask_variable[word_recover]
+    batch_size,seq_len = gold_variable.size(0),gold_variable.size(1)
+    mask = mask_variable.cpu().data.numpy()
+    pred_tag = pred_variable.cpu().data.numpy()
+    gold_tag = gold_variable.cpu().data.numpy()
+    batch_size = mask.shape[0]
+    pred_label,gold_label = [],[]
+    for idx in range(batch_size):
+        pred = [label_alphabet.get_instance(pred_tag[idx][idy]) for idy in range(seq_len) if mask[idx][idy] != 0]
+        gold = [label_alphabet.get_instance(gold_tag[idx][idy]) for idy in range(seq_len) if mask[idx][idy] != 0]
+        assert(len(pred)==len(gold))
+        pred_label.append(pred)
+        gold_label.append(gold)
+    return pred_label, gold_label
+
+
+def recover_nbest_label(pred_variable, mask_variable, label_alphabet, word_recover):
+    """
+        input:
+            pred_variable (batch_size, sent_len, nbest): pred tag result
+            mask_variable (batch_size, sent_len): mask variable
+            word_recover (batch_size)
+        output:
+            nbest_pred_label list: [batch_size, nbest, each_seq_len]
+    """
+    # exit(0)
+    pred_variable = pred_variable[word_recover]
+    mask_variable = mask_variable[word_recover]
+    batch_size,seq_len,nbest = pred_variable.size(0),pred_variable.size(1),pred_variable.size(2)
+
+    mask = mask_variable.cpu().data.numpy()
+    pred_tag = pred_variable.cpu().data.numpy()
+    batch_size = mask.shape[0]
+    pred_label = []
+    for idx in range(batch_size):
+        pred = []
+        for idz in range(nbest):
+            each_pred = [label_alphabet.get_instance(pred_tag[idx][idy][idz]) for idy in range(seq_len) if mask[idx][idy] != 0]
+            pred.append(each_pred)
+        pred_label.append(pred)
+    return pred_label
+
+if __name__ == '__main__':
+    a = np.arange(9.0)
+    print(a)
+    print(norm2one(a))
+2
